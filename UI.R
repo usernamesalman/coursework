8f1a01a5f3645fac1ff5a45a859feb9f23af798e
Bajishaida <bajishaida108@gmail.com>
2015-07-16 17:13:11 +0530

    15-07-2015

--- UI.R
+++ UI.R
@@ -1,13 +1,105 @@
-setwd("D:/salman/coursework")
-setwd("D:/salman/coursework")
+#ui
+
+
+setwd("D:/rshiny")
+
 
 
 library(shiny)
+
+
 shinyUI(fluidPage(
-  titlePanel(title="This is my first app, hello shiny"),
-  sidebarLayout(position="right",
-    sidebarPanel(h3("This is the side bar panel"),h4("widget4"),h5("widget5")),
-    mainPanel(h4("This is the main panel text,output is displayed here"),
-                      h5("this is the output5"))
-  )
-))
+        
+        titlePanel("Hello Shiny"),
+        sidebarLayout(
+                sidebarPanel(),
+                mainPanel()
+        )
+)
+)
+##
+
+## text input shiny widgets
+library(shiny)
+
+
+shinyUI(fluidPage(
+        
+        titlePanel("widgets"),
+        sidebarLayout(
+                sidebarPanel(("enter info"),
+                             textInput(inputId="Name",label="Enter Your Name",value=" "),
+                             textInput(inputId="Age",label="Enter Your Age",value=" ")),
+                mainPanel(("person information"),
+                          textOutput("myName"),
+                          textOutput("myAge")
+                )
+        )
+)
+)
+
+
+###
+## radiobuttons
+
+library(shiny)
+
+
+shinyUI(fluidPage(
+        
+        titlePanel("widgets"),
+        sidebarLayout(
+                sidebarPanel(
+                        ("enter information"),
+                        textInput(inputId="Name",label="Enter Your Name",value=" "),
+                        textInput(inputId="Age",label="Enter Your Age",value=" "),
+                        radioButtons("Gender","Select the Gender",list("Male","Female"))),
+                mainPanel(("person information"),
+                          textOutput("myName"),
+                          textOutput("myAge"),
+                          textOutput("myGender")
+                )
+                
+        )
+)
+)
+
+###
+
+## how to use sliderInput widgets in shiny
+
+
+library(shiny)
+
+shinyUI(fluidPage(
+        titlePanel("sliderInput widgets "),
+        sidebarLayout(
+                sidebarPanel(
+                        sliderInput(inputId="slide",label="select value from slider",min=0,max=100,value=c(20,70))#value=50 )
+                ),
+                mainPanel(
+                        textOutput("out")
+                )
+        )
+)
+)
+
+##  select input Ui widget
+
+
+library(shiny)
+
+shinyUI(fluidPage(
+        titlePanel("sliderInput widgets "),
+        sidebarLayout(
+                sidebarPanel(
+                        selectInput(inputId="Statenames",label="Select the state",c("Delhi","Bangaluru","Chennai","Patna","Tamil Nadu"),selected="Patna",selectize = T,multiple=T)
+                ),
+                mainPanel(
+                        textOutput("state")
+                )
+        )
+)
+)
+
+# how to plot using renderPlot() 
