4620362d8f2592d20306e1a96a92fed1e38f66a8
Bajishaida <bajishaida108@gmail.com>
2015-07-16 17:11:59 +0530

    15-07-2015

--- server.R
+++ server.R
@@ -1,7 +1,48 @@
+setwd("D:/rshiny")
+
 library(shiny)
 
-shinyServer(
-  function(input,output){
-    
-  }
+shinyServer(function(input, output) {
+}
 )
+
+
+
+##
+
+setwd("D:/rshiny")
+
+library(shiny)
+
+shinyServer(function(input, output) {
+        output$myName<-renderText(input$Name)
+        output$myAge<-renderText(input$Age)
+        output$myGender<-renderText(input$Gender)
+}
+)
+
+
+
+## how to use sliderInput widgets in shiny
+
+library(shiny)
+shinyServer(function(input,output){
+        #output$out<-renderText(input$slide)
+        output$out<-renderText(
+                paste("You select the value as:",input$slide))
+        
+})
+
+##  select input Ui widget
+
+library(shiny)
+
+shinyServer(function(input, output) {
+        output$state<-renderText(input$Statenames)
+}
+)
+
+##
+## how to plot using renderPlot() 
+
+################################
